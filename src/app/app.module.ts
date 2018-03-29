2b1a17ad468ac76efc2139fa67c7f89415669af6
Miaan Kramer <miaan@provar.co.za>
2018-03-29 11:00:28 +0200

    First Commit: General Structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { LocationsComponent } from './locations/locations.component';
+import { SettingsComponent } from './settings/settings.component';
+import { HomeComponent } from './home/home.component';
+import { LocationModalComponent } from './location-modal/location-modal.component';
+import { AppRoutingModule } from './/app-routing.module';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LocationsComponent,
+    SettingsComponent,
+    HomeComponent,
+    LocationModalComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
